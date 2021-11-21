22332c3537425d041b361d78be1a806c58efbfaa
jack_xin <xinyijack@qq.com>
2021-11-21 21:30:07 +0800

    第七周作业 学号：G20210760020092 姓名：辛逸杰

---
+++ src/SeventhWeek_Work/Jump_Game/Solution.java
@@ -0,0 +1,20 @@
+package SeventhWeek_Work.Jump_Game;
+
+/**
+ * 55. 跳跃游戏
+ */
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
